5b857189dabe729794a0a74462d66af76e424a68
RichardArce <richard.arce.vargas@gmail.com>
2026-05-18 20:40:57 -0600

    Implementacion del Repositorio de Carro

---
+++ WebAvanzadaIICuatrimestre.DAL/Repositorios/Carro/CarroRepositorio.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAvanzadaIICuatrimestre.DAL.Data;
+
+namespace WebAvanzadaIICuatrimestre.DAL.Repositorios.Carro
+{
+    public class CarroRepositorio : ICarroRepositorio
+    {
+
+        //INYECCION DE DEPENDENCIAS
+        private readonly ApplicationDbContext _context; 
+
+        public CarroRepositorio(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateCarro(Entidades.Carro carro)
+        {
+            if (carro == null) return false;
+            await _context.Carros.AddAsync(carro);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteCarro(int id)
+        {
+            var entity = await _context.Carros.FindAsync(id);
+            if (entity == null) return false;
+
+            _context.Carros.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Entidades.Carro?> GetCarroById(int id)
+        {
+            // Use FindAsync which returns null if not found.
+            return await _context.Carros.FindAsync(id);
+        }
+
+        public async Task<List<Entidades.Carro>> GetCarros()
+        {
+            return await _context.Carros.ToListAsync();
+        }
+
+        public async Task<bool> UpdateCarro(Entidades.Carro carro)
+        {
+            if (carro == null) return false; //Validaciones básicas
+            var existing = await _context.Carros.FindAsync(carro.Id);
+            if (existing == null) return false; //validaciones básicas
+
+            // Update fields
+            existing.Nombre = carro.Nombre;
+            existing.Marca = carro.Marca;
+            existing.Fkduenno = carro.Fkduenno;
+
+            _context.Carros.Update(existing);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ WebAvanzadaIICuatrimestre.DAL/Repositorios/Duenno/IDuennoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAvanzadaIICuatrimestre.DAL.Repositorios.Duenno
+{
+    internal interface IDuennoRepositorio
+    {
+    }
+}
+
+//CRUD
+//Create
+//Read
+//Update
+//Delete
